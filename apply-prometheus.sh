bda003bdb90c130630d98add4ec033845a6bf39a
Huub Daems <hdaems@redhat.com>
2020-06-05 11:32:57 +0200

    mgmt and dev projects.

--- apply-prometheus.sh
+++ apply-prometheus.sh
@@ -2,17 +2,17 @@
 
 set -x
 
-NAMESPACE=prommie
+MGMT_NAMESPACE=fuse-mgmt
 
 DEV_NAMESPACE=fuse7
 
-oc create cm prom-config  --from-file=prometheus.yml=prometheus-config.yml
+oc create cm prom-config --from-file=prometheus.yml=prometheus-config.yml -n ${MGMT_NAMESPACE}
 
-oc create cm alert-config  --from-file=alertmanager.yml=alertmanager-config.yml
+oc create cm alert-config  --from-file=alertmanager.yml=alertmanager-config.yml -n ${MGMT_NAMESPACE}
 
-oc adm policy add-role-to-user view system:serviceaccount:${NAMESPACE}:prom -n ${DEV_NAMESPACE}
+oc adm policy add-role-to-user view system:serviceaccount:${MGMT_NAMESPACE}:prom -n ${DEV_NAMESPACE}
 
 
-oc process -f prometheus-standalone.yaml | oc apply -n ${NAMESPACE} -f -
+oc process -f prometheus-standalone.yaml | oc apply -n ${MGMT_NAMESPACE} -f -
 
 
