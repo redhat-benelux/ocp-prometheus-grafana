04b1b3e1c9569f0de499b474319736b446b42ae9
Huub Daems <hdaems@redhat.com>
2020-06-05 13:12:01 +0200

    Also works under v3.

--- 00_deploy.sh
+++ 00_deploy.sh
@@ -92,11 +92,11 @@ oc apply -f rbac.yml -n ${PrometheusNamespace}
 
 
 
-# note this is for v4, please change it for v3
+# note this is for v4, please change it for v3, not need to change, also works under v3
 export PROM_URL=$(oc get route prom -n ${PrometheusNamespace} -o 'jsonpath={.status.ingress[0].host}')
 
 PROM_URL="https://${PROM_URL}"
 
-echo ${PROM_URL}
+echo "Prometheus : ${PROM_URL}"
 
 exit;
