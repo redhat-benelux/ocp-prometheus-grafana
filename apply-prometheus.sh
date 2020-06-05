202264fc385707cfbc0a1ec594804940851c41d9
Huub Daems <hdaems@redhat.com>
2020-06-05 11:26:47 +0200

    cluster-role view on dev namespace.

--- apply-prometheus.sh
+++ apply-prometheus.sh
@@ -2,12 +2,17 @@
 
 set -x
 
+NAMESPACE=prommie
+
+DEV_NAMESPACE=fuse7
 
 oc create cm prom-config  --from-file=prometheus.yml=prometheus-config.yml
 
 oc create cm alert-config  --from-file=alertmanager.yml=alertmanager-config.yml
 
+oc adm policy add-role-to-user view system:serviceaccount:${NAMESPACE}:prom -n ${DEV_NAMESPACE}
 
-oc process -f prometheus-standalone.yaml | oc apply -f -
+
+oc process -f prometheus-standalone.yaml | oc apply -n ${NAMESPACE} -f -
 
 
